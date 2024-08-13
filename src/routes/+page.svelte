480b1ad50fde7229a2f0fe998412d8395a6f079a
mattcroat <mattcroat.dev@gmail.com>
2024-08-13 18:10:40 +0200

    chore: update example

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,33 +1,50 @@
 <script lang="ts">
-	import { animate, tween, all } from '$lib/index.js'
+	import { tween } from '$lib/index.js'
 	import { formatNumber } from './utils.js'
 
 	const sfx = {
-		transition: 'sfx/transition',
-		tally: 'sfx/tally',
+		transition: 'sfx/transition.mp3',
+		tally: 'sfx/tally.mp3',
 	}
 
 	const svg = tween({ x: -2, y: -2, w: 24, h: 24 })
 	const circle = tween({ x: 2.5, y: 2.5, r: 1.5, fill: '#00ffff' })
 	const text = tween({ count: 0, opacity: 0 })
 
-	animate(async () => {
+	async function animate() {
 		await svg.sfx(sfx.transition).to({ x: 0, y: 0, w: 10, h: 10 })
-		all(
-			circle.sfx(sfx.transition).to({ x: 10, y: 10, r: 3, fill: '#ffff00' }),
-			svg.to({ x: 5, y: 5 })
-		)
-		await text
-			.to({ opacity: 1 }, { duration: 300 })
-			.sfx(sfx.tally)
-			.to({ count: 10_000 }, { duration: 600 })
-	})
+		circle.sfx(sfx.transition).to({ x: 10, y: 10, r: 3, fill: '#ffff00' })
+		svg.to({ x: 5, y: 5 })
+		await text.to({ opacity: 1 }, { duration: 300 })
+		await text.sfx(sfx.tally).to({ count: 10_000 }, { duration: 600 })
+	}
 </script>
 
-<svg viewBox="{$svg.x} {$svg.y} {$svg.w} {$svg.h}">
+<svg viewBox="{svg.x} {svg.y} {svg.w} {svg.h}">
+	{@render grid()}
+
+	<circle cx={circle.x} cy={circle.y} r={circle.r} fill={circle.fill} />
+
+	<text
+		x={circle.x}
+		y={circle.y}
+		font-size={circle.r * 0.4}
+		opacity={text.opacity}
+		text-anchor="middle"
+		dominant-baseline="middle"
+		fill="#000"
+	>
+		{formatNumber(text.count)}
+	</text>
+</svg>
+
+<div>
+	<button onclick={animate}>Animate</button>
+</div>
+
+{#snippet grid()}
 	<g>
 		<g>
-			<!-- Grid lines -->
 			{#each { length: 20 } as _, i}
 				<line
 					x1={1 * i}
@@ -50,7 +67,6 @@
 		</g>
 
 		<g>
-			<!-- Main lines -->
 			{#each { length: 21 } as _, i}
 				{#if i % 5 === 0}
 					<line
@@ -75,7 +91,6 @@
 		</g>
 
 		<g>
-			<!-- Grid numbers -->
 			{#each [0, 5, 10, 15, 20] as number, i}
 				<text
 					x={5 * i}
@@ -100,25 +115,22 @@
 			{/each}
 		</g>
 	</g>
-
-	<circle cx={$circle.x} cy={$circle.y} r={$circle.r} fill={$circle.fill} />
-
-	<text
-		x={$circle.x}
-		y={$circle.y}
-		font-size={$circle.r * 0.4}
-		opacity={$text.opacity}
-		text-anchor="middle"
-		dominant-baseline="middle"
-		fill="#000"
-	>
-		{formatNumber($text.count)}
-	</text>
-</svg>
+{/snippet}
 
 <style>
 	svg {
 		width: 600px;
 		height: 600px;
+	}
+
+	div {
+		margin-top: 2rem;
+		text-align: center;
+	}
+
+	button {
+		font: inherit;
+		font-size: 1.25rem;
+		padding: 0.5rem;
 	}
 </style>
