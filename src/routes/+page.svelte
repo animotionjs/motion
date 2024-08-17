7f349bd8d5dc88cc76df2c760508960017484090
mattcroat <mattcroat.dev@gmail.com>
2024-08-17 14:17:16 +0200

    chore: update example

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -7,20 +7,20 @@
 		tally: 'sfx/tally.mp3',
 	}
 
-	const svg = tween({ x: -2, y: -2, w: 24, h: 24 })
+	const camera = tween({ x: -2, y: -2, w: 24, h: 24 })
 	const circle = tween({ x: 2.5, y: 2.5, r: 1.5, fill: '#00ffff' })
 	const text = tween({ count: 0, opacity: 0 })
 
 	async function animate() {
-		await svg.sfx(sfx.transition).to({ x: 0, y: 0, w: 10, h: 10 })
+		await camera.sfx(sfx.transition).to({ x: 0, y: 0, w: 10, h: 10 })
 		circle.sfx(sfx.transition).to({ x: 10, y: 10, r: 3, fill: '#ffff00' })
-		svg.to({ x: 5, y: 5 })
+		camera.to({ x: 5, y: 5 })
 		await text.to({ opacity: 1 }, { duration: 300 })
-		await text.sfx(sfx.tally).to({ count: 10_000 }, { duration: 600 })
+		text.sfx(sfx.tally).to({ count: 10_000 }, { duration: 600 })
 	}
 </script>
 
-<svg viewBox="{svg.x} {svg.y} {svg.w} {svg.h}">
+<svg viewBox="{camera.x} {camera.y} {camera.w} {camera.h}">
 	{@render grid()}
 
 	<circle cx={circle.x} cy={circle.y} r={circle.r} fill={circle.fill} />
@@ -45,6 +45,7 @@
 {#snippet grid()}
 	<g>
 		<g>
+			<!-- grid lines -->
 			{#each { length: 20 } as _, i}
 				<line
 					x1={1 * i}
@@ -67,6 +68,7 @@
 		</g>
 
 		<g>
+			<!-- main lines -->
 			{#each { length: 21 } as _, i}
 				{#if i % 5 === 0}
 					<line
@@ -91,6 +93,7 @@
 		</g>
 
 		<g>
+			<!-- grid numbers -->
 			{#each [0, 5, 10, 15, 20] as number, i}
 				<text
 					x={5 * i}
